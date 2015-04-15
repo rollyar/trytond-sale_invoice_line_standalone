24cdf90da8a523852f85e7932fabbdc32b141d2f
Guillem Barba <guillem@nan-tic.com>
2015-04-15 19:02:58 +0200

    Allow to avoid restriction to don't delete sale's invoice lines

--- invoice.py
+++ invoice.py
@@ -51,7 +51,8 @@ class InvoiceLine:
     @classmethod
     def delete(cls, lines):
         SaleLine = Pool().get('sale.line')
-        if any(l for l in lines
-                if isinstance(l.origin, SaleLine) and l.type == 'line'):
+        if (not Transaction().context.get('allow_remove_sale_invoice_lines')
+                and any(l for l in lines
+                    if isinstance(l.origin, SaleLine) and l.type == 'line')):
             cls.raise_user_error('delete_sale_invoice_line')
         super(InvoiceLine, cls).delete(lines)
