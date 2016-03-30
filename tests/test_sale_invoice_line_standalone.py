baf447a6081a4181a9fe16c548a43d939028c43c
Raimon Esteve <resteve@zikzakmedia.com>
2016-03-30 10:38:57 +0200

    Reactive RST tests scenarios

--- tests/test_sale_invoice_line_standalone.py
+++ tests/test_sale_invoice_line_standalone.py
@@ -6,6 +6,7 @@ import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
 class SaleInvoiceLineStandaloneTestCase(ModuleTestCase):
@@ -20,5 +21,6 @@ def suite():
     suite.addTests(doctest.DocFileSuite(
             'scenario_sale_invoice_line_standalone.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
